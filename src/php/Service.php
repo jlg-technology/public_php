9e9e3bb59bbc1dad54a327fc7ae9644b43fd5b46
Francis <Francis.bryson22@gmail.com>
2020-04-13 15:52:22 +0100

    KA-2881 Started work on service create functions

--- src/php/Service.php
+++ src/php/Service.php
@@ -2,7 +2,161 @@
 
 namespace jlgtechnology;
 
+use GuzzleHttp\{
+    Client as GuzzleClient,
+    RequestOptions as GuzzleRequestOptions
+};
+
+use GuzzleHttp\Psr7\Response as GuzzleResponse;
+
+use GuzzleHttp\Exception\{
+    ClientException,
+    ServerException
+};
+
+use \Exception as Exception;
+
 class Service
 {
-    
+    private const CRM_AUTH_ENDPOINT = 
+        "https://auth.just-cashflow.com/oauth/token";
+    private const CRM_API_URL = "https://api.crm.dev.jlg-technology.com";
+
+    private $_strJWT;
+
+    private function __construct(string $strJWT)
+    {
+        $this->_strJWT = $strJWT;
+    }
+
+    public static function createFromToken($strJWT) : self
+    {
+        return new self($strJWT);
+    }
+
+    public static function createFromCredentials(
+        string $strClientId,
+        string $strSecret
+    ) : self
+    {
+        $strUrl = self::CRM_AUTH_ENDPOINT;
+
+        $strMethod = "POST";
+
+        $arrHeaders = [
+            "Content-Type" => "application/json"
+        ];
+
+        $arrData = [
+            "grant_type"    => "client_credentials",
+            "client_id"     => $strClientId,
+            "client_secret" => $strSecret,
+            "audience"      => self::CRM_API_URL
+        ];
+
+        $guzzleResponse = self::_makeRequest(
+            $strUrl,
+            $strMethod,
+            $arrHeaders,
+            $arrData
+        );
+
+        $arrResponse = self::_decodeJSONResponse($guzzleResponse);
+
+        exit(var_dump($arrResponse));
+    }
+
+    private static function _makeRequest(
+        string $strUrl,
+        string $strMethod,
+        array $arrHeaders = [],
+        array $arrData = []
+    ) : GuzzleResponse
+    {
+        $arrOptions = [];
+
+        if ($arrHeaders) {
+            $arrOptions[GuzzleRequestOptions::HEADERS] = $arrHeaders;
+        }
+
+        /**
+         * If the content-type is json then we need to set it specially
+         */
+        if (
+            $arrData &&
+            $arrHeaders && 
+            (
+                in_array("Content-Type: application/json", $arrHeaders) ||
+                (
+                    array_key_exists("Content-Type", $arrHeaders) && 
+                    $arrHeaders["Content-Type"] === "application/json"
+                )
+            )
+        ) {
+            $arrOptions[GuzzleRequestOptions::JSON] = $arrData;
+        } else {
+            $arrOptions[GuzzleRequestOptions::FORM_PARAMS] = $arrData;
+        }
+
+        try {
+            $guzzleResponse = $this->_guzzleClient->request(
+                $strMethod,
+                $strUrl,
+                $arrOptions
+            );
+
+        } catch (ClientException $ex) {
+            throw new Exception(
+                sprintf(
+                    "Client error returned from %s (%s)",
+                    $strURL,
+                    $ex->getResponse()->getReasonPhrase()
+                ), 
+                $ex->getResponse()->getStatusCode()
+            );
+
+        } catch (ServerException $ex) {
+            throw new Exception(
+                sprintf(
+                    "Server error returned from %s (%s)",
+                    $strURL,
+                    $ex->getResponse()->getReasonPhrase()
+                ), 
+                $ex->getResponse()->getStatusCode()
+            );
+        }
+
+        return $guzzleResponse;
+    }
+
+    private static function _decodeJSONResponse(
+        GuzzleResponse $guzzleResponse
+    ) : array
+    {        
+        $objBody = $guzzleResponse->getBody();
+
+        /**
+         * Rewind the body contents to the start
+         */
+        $objBody->seek(0);
+
+        $arrResponse = json_decode($objBody->getContents(), true);
+
+        /**
+         * Check the response was proper
+         */
+        if (!$arrResponse) {
+            throw new Exception(
+                "Unable to decode results from server",
+                415
+            );
+        }
+
+        return $arrResponse;
+    }
 }
+
+$objService = Service::createFromCredentials(
+    "JJnSJT0ibfYn6yiiwM6Boy0hnes81sPR",
+    "gBGRAW-2WXT3nNepwxLSjEVuCsf22Qhn4YZUWHJYwFy7_fH20f8JCiHqJrSgrXBG"
+);
