cd45b6e1699c9cd6f754ac13cc16fd7262cebffd
Francis <Francis.bryson22@gmail.com>
2020-04-15 11:17:35 +0100

    KA-2885 Added validation for sic codes

--- src/php/Model/Company.php
+++ src/php/Model/Company.php
@@ -49,6 +49,7 @@ class Company extends AbstractModel
         "|ASCN|STHL|TDCU|BBND|[BFS]IQQ|PCRN|TKCA) ?[0-9][A-Z]{2}|BFPO ?" .
         "[0-9]{1,4}|(KY[0-9]|MSR|VG|AI)[ -]?[0-9]{4}|[A-Z]{2} ?[0-9]{2}|GE ?" .
         "CX|GIR ?0A{2}|SAN ?TA1))\s*$/i";
+    const SIC_CODES_REGEX = "/^\d{5}(,\s?\d{5})*$/";
 
     private const INCORPORATION_DATE_FORMAT = "Y-m-d H:i:s";
 
@@ -82,6 +83,16 @@ class Company extends AbstractModel
         }
 
         if (
+            !is_null($strSicCodes) &&
+            !preg_match(self::SIC_CODES_REGEX, $strSicCodes)
+        ) {
+            throw new Exception(
+                "'$strSicCodes' is not a valid comma seperated list " .
+                    "of sic codes"
+            );
+        }
+
+        if (
             !is_null($intLegalStatus) &&
             !in_array(
                 $intLegalStatus,
@@ -96,7 +107,7 @@ class Company extends AbstractModel
             )
         ) {
             throw new Exception(
-                "'$intLegalStatus' is not a valid legal status"
+                "'$intLegalStatus' is not a valid legal status id"
             );
         }
 
