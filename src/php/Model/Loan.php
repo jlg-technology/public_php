ecbd183daed44a884fe12f53d1a9622cc3c72736
Francis <Francis.bryson22@gmail.com>
2020-04-16 14:44:32 +0100

    KA-2904 Added files array field to loan model

--- src/php/Model/Loan.php
+++ src/php/Model/Loan.php
@@ -9,20 +9,46 @@ class Loan extends AbstractModel
 {
     const FIELD_AMOUNT = "FacilityAmount";
     const FIELD_USE    = "FacilityUse";
+    const FIELD_FILES  = "Files";
 
     public static function create(
         int $intAmount,
-        string $strUse
+        string $strUse,
+        ?array $arrModelFiles
     ) : self
     {
         if ($intAmount < 0) {
             throw new Exception("'$intAmount' is not a valid facility amount");
         }
 
+        if (!is_null($arrModelFiles)) {
+            foreach ($arrModelFiles as $key => $modelFile) {
+                if (!($modelFile instanceof File)) {
+                    throw new Exception(
+                        "An element of the file array is not a file model - " . 
+                            "element is at positon $key with value " . 
+                            print_r($modelFile, true)
+                    );
+                } else if (
+                    !in_array(
+                        $modelFile->getCategoryId(), 
+                        File::LOAN_CATEGORIES
+                    )
+                ) {
+                    throw new Exception(
+                        "'" . $modelFile->getCategoryId() . "' " . 
+                            "on file '" . $modelFile->getNameAndPath() . "' " .
+                            "is not a valid loan file category"
+                    );
+                }
+            }
+        }
+
         return new self(
             [
                 self::FIELD_AMOUNT => $intAmount,
-                self::FIELD_USE    => $strUse
+                self::FIELD_USE    => $strUse,
+                self::FIELD_FILES  => $arrModelFiles
             ]
         );
     }
@@ -34,7 +60,8 @@ class Loan extends AbstractModel
     {
         return [
             self::FIELD_AMOUNT,
-            self::FIELD_USE
+            self::FIELD_USE,
+            self::FIELD_FILES
         ];
     }
 
@@ -46,5 +73,31 @@ class Loan extends AbstractModel
     public function getUse() : string
     {
         return $this->_getField(self::FIELD_USE);
+    }
+
+    public function getFiles() : array
+    {
+        return $this->_getField(self::FIELD_FILES, []);
+    }
+
+    public function addFile(File $modelFile) : self
+    {
+        if (
+            !in_array(
+                $modelFile->getCategoryId(), 
+                File::LOAN_CATEGORIES
+            )
+        ) {
+            throw new Exception(
+                "'" . $modelFile->getCategoryId() . "' " .
+                    "is not a valid loan file category"
+            );
+        }
+
+        $arrModelFiles = $this->_getField(self::FIELD_FILES, []);
+
+        $arrModelFiles[] = $modelFile;
+
+        return $this->_setField(self::FIELD_FILES, $arrModelFiles);
     }
 }
--- test/unit/Model/LoanTest.php
+++ test/unit/Model/LoanTest.php
@@ -4,14 +4,24 @@ namespace jlgtechnology\model;
 
 use PHPUnit\Framework\TestCase;
 
+use Mockery;
+
+use \Exception as Exception;
+
 class LoanTest extends TestCase
 {
+    public function tearDown()
+    {
+        Mockery::close();
+    }
+
     public function testGetFields()
     {
         $this->assertEquals(
             [
                 "FacilityAmount",
-                "FacilityUse"
+                "FacilityUse",
+                "Files"
             ],
             Loan::getFields()
         );
@@ -21,10 +31,19 @@ class LoanTest extends TestCase
     {
         $intFacilityAmount = 123;
         $strFacilityUse = "Test";
+        $arrFiles = [
+            Mockery::mock(File::class)
+                ->shouldReceive("getCategoryId")
+                ->withNoArgs()
+                ->andReturn(File::CATEGORY_OLD_FILE_CONTENTS)
+                ->once()
+                ->mock()
+        ];
 
         $modelLoan = Loan::create(
             $intFacilityAmount,
-            $strFacilityUse
+            $strFacilityUse,
+            $arrFiles
         );
 
         $this->assertEquals(
@@ -35,6 +54,71 @@ class LoanTest extends TestCase
         $this->assertEquals(
             $strFacilityUse,
             $modelLoan->getUse()
+        );
+
+        $this->assertEquals(
+            $arrFiles,
+            $modelLoan->getFiles()
+        );
+    }
+
+    public function testCreate_Invalid_Amount()
+    {
+        $intFacilityAmount = -1;
+        $strFacilityUse = "Test";
+        $arrFiles = [
+            Mockery::mock(File::class)
+        ];
+
+        $this->expectException(Exception::class);
+
+        Loan::create(
+            $intFacilityAmount,
+            $strFacilityUse,
+            $arrFiles
+        );
+    }
+
+    public function testCreate_Invalid_File_Class()
+    {
+        $intFacilityAmount = 123;
+        $strFacilityUse = "Test";
+        $arrFiles = [
+            Mockery::mock(Loan::class)
+        ];
+
+        $this->expectException(Exception::class);
+
+        Loan::create(
+            $intFacilityAmount,
+            $strFacilityUse,
+            $arrFiles
+        );
+    }
+
+    public function testCreate_Invalid_File_Category()
+    {
+        $intFacilityAmount = 123;
+        $strFacilityUse = "Test";
+        $arrFiles = [
+            Mockery::mock(File::class)
+                ->shouldReceive("getCategoryId")
+                ->withNoArgs()
+                ->andReturn(File::CATEGORY_VALUATION_AND_TITLE_PLANS)
+                ->twice()
+                ->shouldReceive("getNameAndPath")
+                ->withNoArgs()
+                ->andReturn("Test file")
+                ->once()
+                ->mock()
+        ];
+
+        $this->expectException(Exception::class);
+
+        Loan::create(
+            $intFacilityAmount,
+            $strFacilityUse,
+            $arrFiles
         );
     }
 }
